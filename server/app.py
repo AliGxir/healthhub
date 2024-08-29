93a4179f8cb2ef01c987fec08d1e919204e27562
AliGxir <147768928+AliGxir@users.noreply.github.com>
2024-08-29 14:29:05 -0600

    add AVSs and AVSById routes

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ from functools import wraps
 
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, "app.db")}")
 
 # Local imports
 from config import app, db, api
@@ -19,11 +19,14 @@ from models.billing import Billing
 from models.avs import AVS
 from schemas.patient_schema import PatientSchema
 from schemas.doctor_schema import DoctorSchema
+from schemas.appointment_schema import AppointmentSchema
 
 patients_schema = PatientSchema(many=True, session=db.session)
 patient_schema = PatientSchema(many=True, session=db.session)
 doctors_schema = DoctorSchema(many=True, session=db.session)
 doctor_schema = DoctorSchema(many=True, session=db.session)
+appointments_schema = AppointmentSchema(many=True, session=db.session)
+appointment_schema = AppointmentSchema(many=True, session=db.session)
 
 class Patients(Resource):
     def get(self):
@@ -35,7 +38,7 @@ class PatientById(Resource):
         if patient := db.session.get(Patient, id):
             patient_schema = PatientSchema()
             return patient_schema.dump(patient), 200
-        return {'error': 'Could not find that patient'}, 404
+        return {"error": "Could not find that patient"}, 404
     
     def patch(self, id):
         if patient := db.session.get(Patient, id):
@@ -50,19 +53,19 @@ class PatientById(Resource):
                 return patient_schema.dump(updated_patient), 200
             except Exception as e:
                 db.session.rollback()
-                return {'error': str(e)}, 400
-        return {'error': 'Patient not found'}, 404
+                return {"error": str(e)}, 400
+        return {"error": "Patient not found"}, 404
 
     def delete(self, id):
         if patient := db.session.get(Patient, id):
             try:
                 db.session.delete(patient)
                 db.session.commit()
-                return {'message': f'Patient #{id} has been deleted'}, 200
+                return {"message": f"Patient #{id} has been deleted"}, 200
             except Exception as e:
                 db.session.rollback()
-                return {'error': str(e)}, 400
-        return {'error': 'Could not find patient'}, 404
+                return {"error": str(e)}, 400
+        return {"error": "Could not find patient"}, 404
     
 class Doctors(Resource):
     def get(self):
@@ -74,13 +77,67 @@ class DoctorById(Resource):
         if doctor := db.session.get(Doctor, id):
             doctor_schema = DoctorSchema()
             return doctor_schema.dump(doctor), 200
-        return {'error': 'Could not find that doctor'}, 404
+        return {"error": "Could not find that doctor"}, 404
+class Appointments(Resource):
+    def get(self):
+        appointments =  appointments_schema.dump(Appointment.query)
+        return appointments, 200
+    
+class AppointmentById(Resource):
+    def get(self, id):
+        if  appointment := db.session.get(Appointment, id):
+            appointment_schema =  AppointmentSchema()
+            return  appointment_schema.dump( appointment), 200
+        return {"error": "Could not find that appointment"}, 404
+    
+    def patch(self, id):
+        if appointment := db.session.get(Appointment, id):
+            try:
+                data = request.json
+                # Validate data
+                appointment_schema.validate(data)
+                # Deserialize data and allow for partial updates
+                updated_appointment = appointment_schema.load(data, instance=appointment, partial=True)
+                db.session.commit()
+                # Serialize the data and package your JSON response
+                return appointment_schema.dump(updated_appointment), 200
+            except Exception as e:
+                db.session.rollback()
+                return {"error": str(e)}, 400
+        return {"error": "Appointment not found"}, 404
+
+    def delete(self, id):
+        if appointment := db.session.get(Appointment, id):
+            try:
+                db.session.delete(appointment)
+                db.session.commit()
+                return {"message": f"Appointment #{id} has been deleted"}, 200
+            except Exception as e:
+                db.session.rollback()
+                return {"error": str(e)}, 400
+        return {"error": "Could not find appointment"}, 404
+
+class AVSs(Resource):
+    def get(self):
+        avss = avss_schema.dump(AVS.query)
+        return avss, 200
+    
+class AVSById(Resource):
+    def get(self, id):
+        if avs := db.session.get(AVS, id):
+            avs_schema = AVSSchema()
+            return avs_schema.dump(avs), 200
+        return {"error": "Could not find that avs"}, 404
     
 
 api.add_resource(Patients, "/patients")
 api.add_resource(PatientById, "/patient/<int:id>")
 api.add_resource(Doctors, "/doctors")
 api.add_resource(DoctorById, "/doctor/<int:id>")
+api.add_resource(Appointments, "/appointments")
+api.add_resource(AppointmentById, "/appointment/<int:id>")
+api.add_resource(AVSs, "/avss")
+api.add_resource(AVSById, "/avs/<int:id>")
 
 
 if __name__ == "__main__":
