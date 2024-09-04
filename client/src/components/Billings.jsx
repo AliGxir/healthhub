a009e890e2a9669258b7fce32ead04a54fd93194
AliGxir <147768928+AliGxir@users.noreply.github.com>
2024-09-04 16:07:08 -0600

    add route protection and fetch call for billings

--- client/src/components/Billings.jsx
+++ client/src/components/Billings.jsx
@@ -1,7 +1,39 @@
 import { useEffect, useState } from "react";
-import { Container, Grid } from "semantic-ui-react";
+import { Container, Grid, Card, Header } from "semantic-ui-react";
+import { useNavigate, useOutletContext } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const Billings = () => {
+  const navigate = useNavigate();
+  const { user } = useOutletContext(); 
+  const [bills, setBills] = useState([]);
+
+    useEffect(() => {
+        if (!user) {
+          navigate("/");
+          return;
+        }
+
+    
+
+    const fetchBills = async () => {
+      try {
+        const resp = await fetch("/api/v1/billings");
+        if (resp.ok) {
+          const data = await resp.json();
+          const patientBills = data.filter((bill) => bill.patient_id === user.id);
+          setBills(patientBills);
+        } else {
+          const errorObj = await resp.json();
+          toast.error(errorObj.error);
+        }
+      } catch (error) {
+        toast.error("Failed to fetch bills.");
+      }
+    };
+
+    fetchBills();
+  }, [user, navigate]);
     return <></>
 }
 
