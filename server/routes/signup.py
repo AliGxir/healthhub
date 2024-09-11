933259e7feede2de8f73977f8192bc322bc4b4dd
AliGxir <147768928+AliGxir@users.noreply.github.com>
2024-09-11 16:42:02 -0600

    add IntegrityError exception to signup.py backend

--- server/routes/signup.py
+++ server/routes/signup.py
@@ -1,6 +1,8 @@
 from flask import session, request
 from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
 from schemas.patient_schema import PatientSchema
+from models.patient import Patient  
 from config import db
 
 patient_schema = PatientSchema(session=db.session)
@@ -8,22 +10,26 @@ patient_schema = PatientSchema(session=db.session)
 class Signup(Resource):
     def post(self):
         try:
-            # Get user input data
             data = request.json
-            # Validate patient information
-            # patient_schema.validate(data)
-            # Create new patient schema
+            
+            existing_patient = db.session.query(Patient).filter_by(email=data.get("email")).first()
+            if existing_patient:
+                return {"error": "Email already exists. Please use a different email."}, 400
+
             new_patient = patient_schema.load(data)
-            # Hash password
-            new_patient.password_hash = request.json.get("password_hash")
+            new_patient.password_hash = request.json.get("password_hash") 
+
             db.session.add(new_patient)
             db.session.commit()
-            # Add patient id to cookies
-            session['patient_id'] = new_patient.id
+
+            session["patient_id"] = new_patient.id
             serialized_patient = patient_schema.dump(new_patient)
             return serialized_patient, 201
+
+        except IntegrityError as e:
+            db.session.rollback()
+            return {"error": "This email is already registered. Please use another email address."}, 400
+
         except Exception as e:
             db.session.rollback()
-            return {'error': str(e)}, 400
-
-
+            return {"error": str(e)}, 400
